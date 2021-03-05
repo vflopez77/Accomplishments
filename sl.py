a5faa6a163cf1debed8364b349e4ab4760f9ac62
vflopez77 <68453606+vflopez77@users.noreply.github.com>
2021-03-04 19:00:50 -0500

    Updating String Length Tester

--- sl.py
+++ sl.py
@@ -3,13 +3,15 @@
 import sys
 
 # Get length of passed-in string
-stringlength = len(str(sys.argv[1]))
-print(f'The length of the string "{sys.argv[1]}" is {stringlength}.')
-if stringlength >= 59:
-    if stringlength == 59:
+maxLength = 59
+stringLength = len(str(sys.argv[1]))
+charstr = 'character'
+print(f'The length of the string "{sys.argv[1]}" is {stringLength}.')
+if stringLength >= 59:
+    if stringLength == 59:
         charstr = 'character'
     else:
         charstr = 'characters'
-    print(f'The string is too long.  Please shorten by {stringlength-58} {charstr}.')
+    print(f'The string is too long.  Please shorten by {stringLength-maxLength} {charstr}.')
 else:
     print(f'The string is not too long.')
