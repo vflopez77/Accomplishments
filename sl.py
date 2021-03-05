2f72657d8c100a2da76e7aab2f58e58d1ecbd27b
vflopez77 <68453606+vflopez77@users.noreply.github.com>
2021-03-04 19:18:36 -0500

    Update sl.py

--- sl.py
+++ sl.py
@@ -7,11 +7,12 @@ maxLength = 59
 stringLength = len(str(sys.argv[1]))
 charstr = 'character'
 print(f'The length of the string "{sys.argv[1]}" is {stringLength}.')
-if stringLength >= 59:
-    if stringLength == 59:
-        charstr = 'character'
+if stringLength > maxLength:
+    if stringLength-maxLength != 1:
+        print(f'The string is too long.  Please shorten by {stringLength-maxLength} {charstr}s.')
     else:
-        charstr = 'characters'
-    print(f'The string is too long.  Please shorten by {stringLength-maxLength} {charstr}.')
-else:
-    print(f'The string is not too long.')
+        print(f'The string is too long.  Please shorten by {stringLength-maxLength} {charstr}.')
+elif abs(stringLength-maxLength) != 1:
+    print(f'The string is not too long with {abs(stringLength-maxLength)} {charstr}s to spare.')
+else: # A single character under
+    print(f'The string is not too long with {abs(stringLength-maxLength)} {charstr} to spare.')
