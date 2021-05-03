7c5bda1d9b7cb71bd8711eb79a6375d4bc845252
vflopez77 <vflopez77@icloud.com>
2021-05-03 14:16:41 -0400

    Adding pace.py and speed.py

---
+++ pace.py
@@ -0,0 +1,4 @@
+speed = input("What is your speed? ")
+pace = 60/float(speed)
+pace = f'{pace:.1f}'
+print(f'Your pace is {pace} minutes per mile.')
---
+++ speed.py
@@ -0,0 +1,4 @@
+pace = input("What is your pace? ")
+speed = 60/float(pace)
+speed = f'{speed:.1f}'
+print(f'Your speed is {speed} miles per hour.')
